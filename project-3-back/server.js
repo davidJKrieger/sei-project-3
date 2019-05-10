f6499b69a2a10168a844b37524999c7e3089281d
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-05-09 19:30:18 -0600

    researched and commented boilerplate server functions

---
+++ project-3-back/server.js
@@ -0,0 +1,39 @@
+//require express
+//express is a framework that makes node.js easier to work with
+const express = require('express');
+//convention is to store the framework in a variable called app
+const app = express();
+//bodyParser is middleware
+//https://www.npmjs.com/package/body-parser
+//The bodyParser object exposes various factories to create middlewares. 
+//All middlewares will populate the req.body property with the parsed body 
+//when the Content-Type request header matches the type option, 
+//or an empty object ({}) if there was no body to parse, 
+//the Content-Type was not matched, or an error occurred.
+const bodyParser = require('body-parser');
+//require cors
+//Cross-Origin Resource Sharing (CORS) 
+//is a mechanism that uses additional HTTP headers to tell a browser 
+//to let a web application running at one origin (domain) 
+//have permission to access selected resources from a server at a different origin.
+const cors = require('cors');
+//require my database files -- that allow node/express server to connect to mongodb 
+//and access modeled data following the strict mongoose "blueprint"
+require('./db/db');
+
+//call the express method that makes bodyParser middleware ready to use -- parses data to json format only
+app.use(bodyParser.json());
+
+const corsOptions = {
+    origin: 'http://localhost:3000', // when you deploy your react app, this is where you put the address,
+    credentials: true, // allowing cookies to be sent with requests from the client (session cookie),
+    optionsSuccessStatus: 200 // some legacy browsers IE11 choke on a 204, and options requests
+}
+
+
+
+//call the express method that will "listen" to (or await requests from) the established environment (.env) or port 9000
+//callback function console logs connection
+app.listen(process.env.PORT || 9000, () => {
+    console.log('listening on port 9000');
+});
